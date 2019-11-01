cb9b68f2c7ae60106041f6221cb9c86593d82d8b
Cirobar <ciro.barbosacosta@gmail.com>
2019-11-01 20:56:31 -0300

    pc3

--- 2_PC/PC3/tof_ultr_stream.c
+++ 2_PC/PC3/tof_ultr_stream.c
@@ -12,26 +12,26 @@
 
 #include <tof.h>
 
-#define TRIG 5   //GPIO 24      
+#define TRIG 5   //GPIO 24
 #define ECHO 6   //GPIO 25
- 
+
 double dist0 = 0.0;
 double dist1 = 0.0;
 int stexec = 0;
- 
+
 void setup() {
        wiringPiSetup();
        pinMode(TRIG, OUTPUT);
        pinMode(ECHO, INPUT);
- 
-} 
- 
+
+}
+
 int tof(){
 	int i;
 	int iDistance;
 	int model, revision;
 
-	// Rpi3 conecta no canal 1 
+	// Rpi3 conecta no canal 1
 	// arg tofInit(int iChan, int iAddr, int bLongRange)
 	i = tofInit(1, 0x29, 1); // modo long range (até 2m)
 	if (i != 1)
@@ -55,19 +55,19 @@ int tof(){
 int ultr1() {
         //Manda pulso trig
         digitalWrite(TRIG, HIGH);
-        delay(0.01);      //delayMicroseconds(20);usleep (20) 
+        delay(0.01);      //delayMicroseconds(20);usleep (20)
         digitalWrite(TRIG, LOW);
- 
-        
+
+
         while(digitalRead(ECHO) == 0); //Espera echo ficar em nível alto
         double startTime = micros(); 	//marcação de tempo
         while(digitalRead(ECHO) == 1); //espera echo acabar
         double endTime = micros(); //tempo do fim do pulso - tempo inicial
         double travelTime = endTime - startTime;
-        
+
         //Conversão em cm
         dist1 = (travelTime/58.0); //
-        
+
         //printf("Distance: %.5lf us\n", travelTime);
         printf("Dist1: %.3lf cm\n", dist1);
         return 2;
@@ -79,27 +79,27 @@ void stream(){
 	char det[35];
 	char buffer_ent[100];
 	char buffer_sai[100];
-	
-	
+
+
 	system("vcgencmd get_camera >> camanalyser.txt");
-	
+
 	fp = open("camanalyser.txt", O_RDONLY);
 
 	while(read(fp,&ch,1) > 0){
   		  det[i] = ch;
-		     i++;	
+		     i++;
 	}
 	close(fp);
 	if (det[21] == '0') { //posição da string que diz se a camera está conectada
 		printf("A camera não está conectada\n Falha em abrir stream\n");
-		system("curl -s -X POST https://api.telegram.org/bot1067349259:AAHQQ2jD7224MYYtyJr8s0aeJONG3aTHqHg/sendMessage -d chat_id=705655937 -d text='A stream falhou em ser executada'");
+		system("curl -s -X POST https://api.telegram.org/bot106.../sendMessage -d chat_id=705... -d text='A stream falhou em ser executada'");
 		system("sudo rm camanalyser.txt"); //remove arquivo de análise do status da câmera
 		exit(-1);
-	
+
 	}
         system("sudo rm camanalyser.txt"); //remove arquivo de análise do status da câmera
 
-	
+
 	strcpy(buffer_ent,"uv4l --driver raspicam --auto-video_nr --width 640 --height 480 --encoding jpeg");
 	//puts(buffer_ent);
 	system(buffer_ent);
@@ -108,11 +108,11 @@ void stream(){
 	//puts(buffer_sai);
 	system(buffer_sai);
 	usleep(500000);
-	
+
 	strcpy(buffer_ent,"Stream executando...para parar aperte ctrl_c\n");
 	puts(buffer_ent);
-	
-	system("curl -s -X POST https://api.telegram.org/bot1067349259:AAHQQ2jD7224MYYtyJr8s0aeJONG3aTHqHg/sendMessage -d chat_id=705655937 -d text='A stream foi aberta'");
+
+	system("curl -s -X POST https://api.telegram.org/bot106../sendMessage -d chat_id=705... -d text='A stream foi aberta'");
 	// envia a mensagem "Stream foi aberta" no telegram
 } //stream
 
@@ -126,7 +126,7 @@ void ctrl_c(int sig)
 
 int main(int argc, char *argv[])
 {
-    
+
     //char ch;
      int fd[2];
     //int fd1[2];
@@ -134,42 +134,42 @@ int main(int argc, char *argv[])
     pid_t pfilho;
     signal(SIGINT, ctrl_c);
     signal(SIGUSR1, stream);
-    
+
     //pipe(fd);
     //pipe(fd1);
-    
+
     pfilho = fork();
-		
+
     if(pfilho == 0){
 	printf("Processo filho\n");
 	while(1){
-	//write(fd[0],&stexec,1);	
-	} 
+	//write(fd[0],&stexec,1);
+	}
     }
     else{
 	while(1){
 	delay(300);  //0.5s
-	tof();	
+	tof();
 	ultr1();
 		if(dist0 > 3 && dist0 < 5){
-			while(stexec==0){ 
-			usleep(300);	
+			while(stexec==0){
+			usleep(300);
 			kill(pfilho,SIGUSR1);
-			//read(fd[0],&stexec,1); 	
+			//read(fd[0],&stexec,1);
 			stexec = 1;
-			system("curl -s -X POST https://api.telegram.org/bot1067349259:AAHQQ2jD7224MYYtyJr8s0aeJONG3aTHqHg/sendMessage -d chat_id=705655937 -d text='A stream foi aberta pelo tof'");
+			system("curl -s -X POST https://api.telegram.org/bot106.../sendMessage -d chat_id=705... -d text='A stream foi aberta pelo tof'");
 			}
 		}
 		if(dist1 > 3 && dist1 < 5){
-			while(stexec==0){ 
-			usleep(300);	
+			while(stexec==0){
+			usleep(300);
 			kill(pfilho,SIGUSR1);
-			//read(fd[0],&stexec,1);	
+			//read(fd[0],&stexec,1);
 			stexec = 1;
-			system("curl -s -X POST https://api.telegram.org/bot1067349259:AAHQQ2jD7224MYYtyJr8s0aeJONG3aTHqHg/sendMessage -d chat_id=705655937 -d text='A stream foi aberta pelo ultr1'");
+			system("curl -s -X POST https://api.telegram.org/bot106.../sendMessage -d chat_id=705... -d text='A stream foi aberta pelo ultr1'");
 			}
-		}   // confere distancia do ultrassom e abre a stream caso necessário (não abre se já estiver aberta) aberta).		
+		}   // confere distancia do ultrassom e abre a stream caso necessário (não abre se já estiver aberta) aberta).
 	}
-    }	
+    }
     return 0;
 }
